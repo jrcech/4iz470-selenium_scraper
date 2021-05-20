71bb86eda3365c7bef37a2529cd291dd6f73b339
Jiří Čech <jiricech94@gmail.com>
2021-05-20 11:09:00 +0200

    Refactoring

--- csfd_spider.rb
+++ csfd_spider.rb
@@ -15,7 +15,7 @@ class CsfdSpider < Kimurai::Base
     response = browser.current_response
 
     response.xpath("//a[@class='film-title-name']").each do |a|
-      request_to :parse_repo_page, url: absolute_url(a[:href], base: url)
+      request_to :parse_movie_page, url: absolute_url(a[:href], base: url)
     end
 
     # next_page = response.at_xpath("//a[@class='next_page']")
@@ -25,29 +25,78 @@ class CsfdSpider < Kimurai::Base
     # end
   end
 
-  def parse_repo_page(response, url:, data: {})
+  def parse_movie_page(response, url:, data: {})
     @response = response
-
     item = {}
 
-    item[:title] = @response.xpath("//h1").text.strip
-    item[:genres] = @response.xpath("//div[@class='genres']").text.split(' / ')
-    item[:origin] = @response.xpath("//div[@class='origin']").text.split(',').first.split(' / ')
-    item[:year] = @response.xpath("//span[@itemprop='dateCreated']").text.strip
-    item[:rating_value] = @response.xpath("//meta[@itemprop='ratingValue']").attribute('content')
-    item[:rating_count] = @response.xpath("//meta[@itemprop='ratingCount']").attribute('content')
-    item[:review_count] = @response.xpath("//meta[@itemprop='reviewCount']").attribute('content')
-    item[:based_on] = @response.xpath("//h4[.='Předloha: ']/following::span[1]/a").text.strip
-    item[:directors] = loop_data "//h4[.='Režie: ']/following::span[1]/a"
-    item[:scenarists] = loop_data "//h4[.='Scénář: ']/following::span[1]/a"
-    item[:actors] = loop_data "//h4[.='Hrají: ']/following::span[1]/a | //h4[.='Hrají: ']/following::span[1]/span[1]/a"
-    item[:musicians] = loop_data "//h4[.='Hudba: ']/following::span[1]/a"
-    item[:producers] = loop_data "//h4[.='Produkce: ']/following::span[1]/a"
+    item[:title] = @response
+      .xpath("//h1")
+      .text
+      .strip
+
+    item[:genres] = @response
+      .xpath("//div[@class='genres']")
+      .text
+      .split(' / ')
+
+    item[:origin] = @response
+      .xpath("//div[@class='origin']")
+      .text
+      .split(',')
+      .first
+      .split(' / ')
+
+    item[:year] = @response
+      .xpath("//span[@itemprop='dateCreated']")
+      .text
+      .strip
+
+    item[:rating_value] = @response
+      .xpath("//meta[@itemprop='ratingValue']")
+      .attribute('content')
+
+    item[:rating_count] = @response
+      .xpath("//meta[@itemprop='ratingCount']")
+      .attribute('content')
+
+    item[:review_count] = @response
+      .xpath("//meta[@itemprop='reviewCount']")
+      .attribute('content')
+
+    item[:based_on] = @response
+      .xpath("//h4[.='Předloha: ']/following::span[1]/a")
+      .text
+      .strip
+
+    item[:directors] = loop_to_array(
+      "//h4[.='Režie: ']/following::span[1]/a | " +
+      "//h4[.='Režie: ']/following::span[1]/span[1]/a"
+    )
+
+    item[:scenarists] = loop_to_array(
+      "//h4[.='Scénář: ']/following::span[1]/a |" +
+      "//h4[.='Scénář: ']/following::span[1]/span[1]/a"
+    )
+
+    item[:actors] = loop_to_array(
+      "//h4[.='Hrají: ']/following::span[1]/a | " +
+      "//h4[.='Hrají: ']/following::span[1]/span[1]/a"
+    )
+
+    item[:musicians] = loop_to_array(
+      "//h4[.='Hudba: ']/following::span[1]/a | " +
+      "//h4[.='Hudba: ']/following::span[1]/span[1]/a"
+    )
+
+    item[:producers] = loop_to_array(
+      "//h4[.='Produkce: ']/following::span[1]/a | " +
+      "//h4[.='Produkce: ']/following::span[1]/span[1]/a"
+    )
 
     save_to "results.json", item, format: :pretty_json
   end
 
-  def loop_data(xpath)
+  def loop_to_array(xpath)
     items = []
 
     @response.xpath(xpath).each do |item|
